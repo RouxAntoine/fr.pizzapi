05620823b41dc1e668f879f03f8531e7940f97ea
Antoine <antoinroux@hotmail.fr>
2018-01-27 23:39:54 +0100

    début configuration du dépot initialisation du projet

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    target: "node",
+    devServer: {
+        host: "localhost",
+        port: 4444,
+        https: true
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
