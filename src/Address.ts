aca23bfc0480e3492d683a2ecae6c3b718b3f41e
mickael <mickael.bettinelli@etu.univ-lyon1.fr>
2018-02-13 21:41:03 +0100

    classe adresse

---
+++ src/Address.ts
@@ -0,0 +1,20 @@
+export class Address {
+    private code: number;
+    private num: string;
+    private street: string;
+
+    constructor(num: string, code: number, street: string) {
+        this.num = num;
+        this.code = code;
+        this.street = street;
+    }
+
+    /**
+     * @param : numéro de rue, nom de la rue, code postal
+     * @returns : true si OK, false si l'adresse n'est pas dans la zone de livraison
+     */
+    public async setDeliveryAddress(num: number, street: string, postalCode: number): Promise<boolean> {
+        //TODO: Enregistre et vérifie l'adresse de l'utilisateur, indique si Dominos peut livrer ici
+        return false;
+    };    
+}
--- src/Index.ts
+++ src/Index.ts
@@ -73,15 +73,6 @@ export class App {
     };
 
     /**
-     * @param : numéro de rue, nom de la rue, code postal
-     * @returns : true si OK, false si l'adresse n'est pas dans la zone de livraison
-     */
-    public async setDeliveryAddress(num: number, street: string, postalCode: number): Promise<boolean> {
-        //TODO: Enregistre et vérifie l'adresse de l'utilisateur, indique si Dominos peut livrer ici
-        return false;
-    };    
-
-    /**
      * @param : id du magasin
      * @returns : json des infos du magasin
      */
